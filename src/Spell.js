c0f22c631e4dd1044f2e64d05b60eecc94ab30b5
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-06-08 15:43:27 -0500

    Install Cypress and complete testing for mainpage

--- src/Spell.js
+++ src/Spell.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Spell.css'
-const Spell = ({name, index}) => {
+const Spell = ({name, index, id}) => {
   return (
     <Link to={`/${index}`} style={{ textDecoration: 'none' }}>
-      <div className='spell'>
+      <div className='spell' id={id}>
         <h2 className='spell-name'>{name}</h2>
         <img className='spell-image'src="https://i.pinimg.com/originals/0f/ec/ce/0feccede3b7b4b18868110d1621bcd88.jpg"/>
       </div>
